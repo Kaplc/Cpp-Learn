0468ba79a53be17da8fdeffff085ccca84a130f1
Kaplc <755536175@qq.com>
2023-05-04 17:01:53 +0800

    完成使用回调函数打印不同数据类型

--- src/link.c
+++ src/link.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "../header/link.h"
 
@@ -21,24 +22,38 @@ typedef struct person {
     char name[64];
     int id;
 } PERSON;
+void print_person(void *data){
+    PERSON *person_data = data;
+    printf("姓名：%s 学号：%d\n", person_data->name, person_data->id);
 
+}
+void print_int(void *data){
+    int *intdata = data;
+    printf("整数：%d\n", *intdata);
+}
+void print_double(void *data){
+    double *doubledata = data;
+    printf("浮点数：%lf\n", *doubledata);
+}
+void print_chars(void *data){
+    char *chardata = data;
+    printf("字符串：%s\n", chardata);
+}
 void create_data(MAINTENANCE *list, void *data) {
-    data = (PERSON *) data; // 转换指针类型
     DATANODE *new_datanode = malloc(sizeof(DATANODE)); // 创建新数据节点
     new_datanode->data = data;
     new_datanode->node.next = NULL;
 
     list->header.node.next = (LINKNODE *) new_datanode; // 指向数据节点的前面属于LINKNODE的内存空间
     list->size++;
-
 }
 
-void read_link(MAINTENANCE *list) {
-    PERSON *data = NULL;
+void read_link(MAINTENANCE *list, void (*print_func)(void *)) {
+
     DATANODE *current = &(list->header);
     for (int i = 0; i < list->size; ++i) {
-        current = (DATANODE *) current->node.next;
-        data = current->data;
+        current = (DATANODE *) current->node.next; // 通过node找到节点首地址，强转成DATANODE *改变指针步长读取所有数据
+        print_func(current->data); // 回调函数处理不同类型数据
     }
 }
 
@@ -49,10 +64,10 @@ void run_link() {
     list->header.data = NULL;
     list->header.node.next = NULL;
     // 输入数据
-    PERSON student = {"xiaoming", 1};
+//    PERSON data = {"xiaoming", 1};
+    int data = 100;
     // 插入链表
-    create_data(list, &student);
+    create_data(list, &data);
     // 查询链表
-    read_link(list);
-
+    read_link(list, print_int);
 }
