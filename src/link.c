1e1713d5cb74a545fb40b6853c935123dba157c2
Kaplc <755536175@qq.com>
2023-05-04 17:52:23 +0800

    完成链表插入

--- src/link.c
+++ src/link.c
@@ -22,34 +22,65 @@ typedef struct person {
     char name[64];
     int id;
 } PERSON;
-void print_person(void *data){
+
+void print_person(void *data) {
     PERSON *person_data = data;
     printf("姓名：%s 学号：%d\n", person_data->name, person_data->id);
 
 }
-void print_int(void *data){
+
+void print_int(void *data) {
     int *intdata = data;
     printf("整数：%d\n", *intdata);
 }
-void print_double(void *data){
+
+void print_double(void *data) {
     double *doubledata = data;
     printf("浮点数：%lf\n", *doubledata);
 }
-void print_chars(void *data){
+
+void print_chars(void *data) {
     char *chardata = data;
     printf("字符串：%s\n", chardata);
 }
-void create_data(MAINTENANCE *list, void *data) {
+
+void create_datanode(MAINTENANCE *list, void *data, int pos) {
+    if (list == NULL) {
+        return;
+    }
     DATANODE *new_datanode = malloc(sizeof(DATANODE)); // 创建新数据节点
     new_datanode->data = data;
     new_datanode->node.next = NULL;
 
-    list->header.node.next = (LINKNODE *) new_datanode; // 指向数据节点的前面属于LINKNODE的内存空间
+    if (pos > list->size) {
+        // 插入位置大于链表长度进行尾插
+        LINKNODE *current = &(list->header.node);
+        for (int i = 0; i < list->size; ++i) { // 移动链表指针到末尾
+            current = current->next;
+        }
+        // 连接链表
+        current->next = (LINKNODE*)new_datanode;// 指向数据节点的前面属于LINKNODE的内存空间
+
+    } else {
+        if (pos < 1)pos = 1; // 小于1修改为头插
+
+        LINKNODE *front = list->header.node.next; // 前驱指针
+        LINKNODE *after = &(list->header.node); // 后驱指针
+        for (int i = 1; i < pos; ++i) {
+            after = front; // 移动后驱
+            front = front->next; // 移动前驱
+        }
+        // 连接链表
+        after->next = (LINKNODE*)new_datanode;
+        new_datanode->node.next = front;
+    }
     list->size++;
 }
 
-void read_link(MAINTENANCE *list, void (*print_func)(void *)) {
-
+void print_link(MAINTENANCE *list, void (*print_func)(void *)) {
+    if (list == NULL) {
+        return;
+    }
     DATANODE *current = &(list->header);
     for (int i = 0; i < list->size; ++i) {
         current = (DATANODE *) current->node.next; // 通过node找到节点首地址，强转成DATANODE *改变指针步长读取所有数据
@@ -64,10 +95,19 @@ void run_link() {
     list->header.data = NULL;
     list->header.node.next = NULL;
     // 输入数据
-//    PERSON data = {"xiaoming", 1};
-    int data = 100;
+    PERSON data1 = {"xiaoming", 1};
+    PERSON data2 = {"xiaohong", 2};
+    PERSON data3 = {"xiaoa", 3};
+    PERSON data4 = {"xiaob", 4};
+    PERSON data5 = {"xiaoc", 5};
+//    int pos = 3;
+//    int data = 100;
     // 插入链表
-    create_data(list, &data);
+    create_datanode(list, &data1, 1);
+    create_datanode(list, &data2, 2);
+    create_datanode(list, &data3, -9);
+    create_datanode(list, &data4, 100);
+    create_datanode(list, &data5, 2);
     // 查询链表
-    read_link(list, print_int);
+    print_link(list, print_person);
 }
