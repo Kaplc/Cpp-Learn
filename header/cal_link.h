417a5511641e1cdac544c276b41f8e2579a9fb1e
Kaplc <755536175@qq.com>
2023-05-11 14:13:36 +0800

    更新：生成中间链表

---
+++ header/cal_link.h
@@ -0,0 +1,8 @@
+//
+// Created by kaplc on 2023/5/11.
+//
+
+#ifndef LINUX_CPPLEARN_CAL_LINK_H
+#define LINUX_CPPLEARN_CAL_LINK_H
+
+#endif //LINUX_CPPLEARN_CAL_LINK_H
---
+++ src/cal_link.c
@@ -0,0 +1,33 @@
+//
+// Created by kaplc on 2023/5/11.
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../header/cal_link.h"
+#include "../header/cal_stack.h"
+
+
+typedef struct linklist {
+    NODE header;
+    int size;
+} LINKLIST;
+
+LINKLIST *init_calLink() {
+    LINKLIST *linklist = malloc(sizeof(LINKLIST));
+    linklist->header.type = 'h';
+    linklist->header.data = NULL;
+    linklist->header.next = NULL;
+    linklist->size = 0;
+    return linklist;
+
+}
+
+void add_node(LINKLIST *linklist, NODE *node) {// 尾插
+    NODE *current = &(linklist->header);
+    while (current->next != NULL) { // 移动到尾部
+        current = current->next;
+    }
+    current->next = node;
+}
+
